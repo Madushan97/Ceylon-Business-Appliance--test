4cc43219392c6589c18458ee2cc86e18469642f6
Madushan97 <madushannipuna1997@gmail.com>
2023-06-22 15:35:39 +0530

    project initiated

---
+++ src/main/java/com/madushan/book/publishing/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.madushan.book.publishing;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BookPublishingApplication.class);
+	}
+
+}
---
+++ src/test/java/com/madushan/book/publishing/BookPublishingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madushan.book.publishing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPublishingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
